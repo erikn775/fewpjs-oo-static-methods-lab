36c4c256674f1c34e337f7c2444d7db4051188f1
erikn775 <erikn775@gmail.com>
2021-02-08 15:29:48 -0700

    commit tree f4c3520a1828a80bf5407ade7c8389ae93186e4c

--- index.js
+++ index.js
@@ -1,3 +1,18 @@
 class Formatter {
-  //add static methods here
+  static capitalize(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+  }
+
+  static sanitize(string) {
+    return string.replace(/[.!*+?^@#${}()|[\]\\]/g, "")
+  }
+
+  static titleize(string) {
+    let newString = string.toLowerCase()
+      .split(' ')
+      .map((s) => s.charAt(0).toUppercase() + s.substring(1))
+      .join(' ');
+  }
 }
+
+
